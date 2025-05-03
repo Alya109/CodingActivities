ee2e90dc46e0169ad36afe7c22a355d90a085086
Alya <149942623+Alya109@users.noreply.github.com>
2025-05-03 15:47:35 +0800

    Update Animals.py

--- Animals.py
+++ Animals.py
@@ -1,1 +1,20 @@
 # Earl Lumabi
+class Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def make_sound(self):
+        print("Meeeehhhhh")
+    def describe(self):
+        print("Prints a description of the animal including its name and age.")
+
+class Lion(Animal):
+    def roar(self):
+        print("Roars loudly.")
+
+class Bird(Animal):
+    def fly(self):
+        print("Flies high in the sky.")
+
+class Reptile(Animal):
+    
